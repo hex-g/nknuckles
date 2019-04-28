9b7f1ad2eece4b5f00023c6b8b809c9b17531ee4
Victor Hirumitsu <vmhirumitsu@gmail.com>
2019-04-28 05:19:44 -0300

    Create from old project + Adjustments

---
+++ src/main/java/hive/nknuckles/security/SecurityTokenConfig.java
@@ -0,0 +1,63 @@
+package hive.nknuckles.security;
+
+import hive.pandora.security.JwtConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+@EnableWebSecurity
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  private JwtConfig jwtConfig;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .cors().and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+        .and()
+        .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+        .authorizeRequests()
+        .antMatchers(HttpMethod.POST, "/caronte" + jwtConfig.getUri()).permitAll()
+        // Role-specific routes
+        .antMatchers("/**/admin/**").hasRole("ADMIN")
+        .antMatchers("/**/pedagogue/**").hasRole("PEDAGOGUE")
+        .antMatchers("/**/student/**").hasRole("STUDENT")
+        // API routes
+        .antMatchers("/kirby/**").authenticated()
+        // Block everything else 'cause I'm paranoid
+        .antMatchers("/**").denyAll();
+  }
+
+  @Bean
+  public JwtConfig jwtConfig() {
+    return new JwtConfig();
+  }
+
+
+  // TODO change this to use properties
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final var config = new CorsConfiguration();
+    config.setAllowedOrigins(Arrays.asList("*"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST"));
+    config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+    config.setExposedHeaders(Arrays.asList("Content-Type", "Authorization"));
+    final var source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
+}
